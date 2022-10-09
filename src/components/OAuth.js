68e5023a664b9915f5cda109e3fce9343ed0dd79
wabibaah <wabisernproject@gmail.com>
2022-10-09 22:27:19 +0000

    added the oauth functionality

--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -1,8 +1,41 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+import { db } from "../firebase";
 
 function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const { user } = result;
+      // check for the user (with google he can use it for both sign in and sign up so you as the programmer must do that background check to see and if he / she is now coming to our website, we can save it into our firestore database)
+      const docRef = doc(db, "users", user.uid); // reference does not go to the database (just a reference)
+      const docSnap = await getDoc(docRef); // getDoc is for reading the document and setDoc is for creating the document
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+      console.log(error);
+    }
+  }
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase  text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md  hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
+    <button
+      onClick={onGoogleClick}
+      type="button"
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase  text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md  hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
       <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
       Continue with Google
     </button>
