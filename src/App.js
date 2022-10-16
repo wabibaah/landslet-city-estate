9c89b8d4d4f019af8b4c9c12036026f9c7806064
wabibaah <wabisernproject@gmail.com>
2022-10-16 12:13:07 +0000

    finished the rent and sale pages dynamically

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import CreateListing from "./pages/CreateListing";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import Listing from "./pages/Listing";
+import Category from "./pages/Category";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/category/:categoryName/:listingId" element={<Listing />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
           </Route>
---
+++ src/pages/Category.js
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
+
+import { collection, getDocs, limit, orderBy, query, startAfter, where } from "firebase/firestore";
+import { db } from "../firebase";
+import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
+
+function Category() {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        const listingRef = collection(db, "listings");
+        const q = query(
+          listingRef,
+          where("type", "==", params.categoryName),
+          orderBy("timeStamp", "desc"),
+          limit(2)
+        );
+        const querySnap = await getDocs(q);
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+        // console.log(lastFetchedListing);
+        const listings = [];
+        querySnap.forEach((listing) =>
+          listings.push({
+            id: listing.id,
+            data: listing.data(),
+          })
+        );
+        setListings(listings);
+        setLoading(false);
+        // console.log(listings);
+      } catch (error) {
+        console.log(error);
+        toast.error("Could not fetch listing");
+      }
+    }
+    fetchListings();
+  }, [params.categoryName]);
+
+  async function onFetchMoreListings() {
+    try {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("type", "==", params.categoryName),
+        orderBy("timeStamp", "desc"),
+        startAfter(lastFetchedListing),
+        limit(2)
+      );
+      const querySnap = await getDocs(q);
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+      // console.log(lastFetchedListing);
+      const listings = [];
+      querySnap.forEach((listing) =>
+        listings.push({
+          id: listing.id,
+          data: listing.data(),
+        })
+      );
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+      // console.log(listings);
+    } catch (error) {
+      console.log(error);
+      toast.error("Could not fetch listing");
+    }
+  }
+
+  return (
+    <div className="max-w-6xl mx-auto px-3">
+      <h1 className="text-3xl text-center mt-6 font-bold mb-6">{`Places for ${params.categoryName}`}</h1>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 ">
+              {listings.map((listing) => (
+                <ListingItem key={listing.id} id={listing.id} listing={listing.data} />
+              ))}
+            </ul>
+          </main>
+          {lastFetchedListing && (
+            <div className="flex justify-center items-center">
+              <button
+                onClick={onFetchMoreListings}
+                className="bg-white px-3 py-1.5 text-gray-700 border border-gray-300 mb-6 mt-6 hover:border-slate-600 rounded transition duration-150 ease-in-out"
+              >
+                Load more
+              </button>
+            </div>
+          )}
+        </>
+      ) : (
+        <p>There are no current offers</p>
+      )}
+    </div>
+  );
+}
+
+export default Category;
+
+// before the automatic will come, you must have already opened it, so it is now like it is looking at it in our file
